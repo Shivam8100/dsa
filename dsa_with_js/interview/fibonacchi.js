1ecb3e7f20aa99d6cc603718fba560cc946b6100
Shivam Ranjan <shivamranjan@Shivams-MacBook-Air.local>
2025-08-15 13:23:25 +0530

    ps-2 new approach added in fibonacci

--- dsa_with_js/interview/fibonacchi.js
+++ dsa_with_js/interview/fibonacchi.js
@@ -1,22 +1,20 @@
 function fibonacci(n){
     const fib=[0,1];
-    for(let i=2;i<n;i++){
+    for(let i=2;i<=n;i++){
         fib[i]=fib[i-1]+fib[i-2]
     }
-    return fib[n-1];
+    return fib[n];
 }
 
 
-console.log("iterative")
-console.log(fibonacci(3))//[ 0, 1, 1 ]
-console.log(fibonacci(6))//[ 0, 1, 1, 2, 3, 5 ]
-// console.log(fibonacci(9))
+console.time("fibonacci");
+console.log(fibonacci(5))//[ 0, 1, 1 ]
+console.timeEnd("fibonacci");
+
+
 
 // resursive approach 
 function fibonacciWithRecursion (n){
-    if(n<0){
-    console.log("provide positive number")
-    }
    if(n<=1){
     return n
    }
@@ -27,7 +25,9 @@ function fibonacciWithRecursion (n){
 
 
 }
-console.log(fibonacciWithRecursion(4),"resursive")
+console.time("Fibonacci with Recursion");
+console.log(fibonacciWithRecursion(5))// 2
+console.timeEnd("Fibonacci with Recursion");
 
 
 
@@ -37,17 +37,46 @@ let a = 0
 let b = 1
 let sum = 0
 let arr = [a,b]
-for(let i = 3 ; i <= n ; i++){
+for(let i = 2 ; i <= n ; i++){
     sum = a + b
     arr.push(sum);
     a = b
     b = sum
 }
-return arr;
+return arr[n];
 
 }
 // console.log("with new approach")
-// console.log(fibonacciWithSwap(10));
+console.time("Fibonacci with Swap");
+console.log(fibonacciWithSwap(5)); // [0, 1, 1, 2, 3]
+console.timeEnd("Fibonacci with Swap");
+
+
+//fib optimized
+var fibOpt = function(n) {
+    if (n < 2) return n;
+
+    let a = 0, b = 1;
+    for (let i = 2; i <= n; i++) {
+        [a, b] = [b, a + b];
+    }
+    return b;
+};
+console.time("Optimized Fibonacci");
+console.log(fibOpt(5)); // 55
+console.timeEnd("Optimized Fibonacci");
+
+
+//results:
+// 5
+// fibonacci: 3.864ms
+// 5
+// Fibonacci with Recursion: 0.029ms
+// 5
+// Fibonacci with Swap: 0.028ms
+// 5
+// Optimized Fibonacci: 0.03ms
+
 
 
 
